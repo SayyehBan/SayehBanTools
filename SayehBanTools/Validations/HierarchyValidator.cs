32d1710f4ea938a6d8ce2de705e0e141e9277cba
سایه بان <38620223+SayyehBan@users.noreply.github.com>
2025-05-20 01:06:55 +0330

    ثبت کلاس بررسی ساختار درختی

---
+++ SayehBanTools/Validations/HierarchyValidator.cs
@@ -0,0 +1,27 @@
+﻿namespace SayehBanTools.Validations;
+
+
+/// <summary>
+/// کلاس بررسی ساختار درختی
+/// </summary>
+public static class HierarchyValidator
+{
+    /// <summary>
+    /// بررسی اعتبار سنجی ساختار درختی
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static bool IsValidHierarchyPath(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+            return true;
+
+        // باید با / شروع و پایان یابد
+        if (!path.StartsWith("/") || !path.EndsWith("/"))
+            return false;
+
+        // بین /ها باید اعداد باشند
+        var parts = path.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+        return parts.All(part => int.TryParse(part, out _));
+    }
+}
