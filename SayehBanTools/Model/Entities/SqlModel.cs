3113861c0c4bc5a56027585bf412eb5716898765
سایه بان <38620223+SayyehBan@users.noreply.github.com>
2025-07-09 15:30:16 +0330

    افوزدن فیلد به Model اسکیوال

--- SayehBanTools/Model/Entities/SqlModel.cs
+++ SayehBanTools/Model/Entities/SqlModel.cs
@@ -38,5 +38,32 @@ public class SqlModel
         /// تعداد رکورد
         /// </summary>
         public int SubCategoryCount { get; set; }
+        /// <summary>
+        /// شناسه
+        /// </summary>
+        public int ID { get; set; }
     }
+    /// <summary>
+    /// پیغام خروجی جدول
+    /// </summary>
+    public class OutputMessageTableType
+    {
+        /// <summary>
+        /// کد خطا
+        /// </summary>
+        public int Code { get; set; }
+        /// <summary>
+        /// پیغام خطا
+        /// </summary>
+        public string Message { get; set; } = string.Empty;
+        /// <summary>
+        /// تعداد
+        /// </summary>
+        public int Count { get; set; }
+        /// <summary>
+        ///  شناسه ها
+        /// </summary>
+        public string? Ids { get; set; }
+    }
+
 }
